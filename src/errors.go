2b9190a0408639b1a2bfd85fa85eaaeae7e1d109
rakshith <rakshith21mohan@gmail.com>
2022-01-23 16:20:54 +0530

    changes to error handling

--- src/errors.go
+++ src/errors.go
@@ -7,12 +7,9 @@ import (
 
 // returns false if validation failed
 func validateSubmitRequest(id, lang string, ctx *gin.Context) bool {
-	if len(id) == 0 {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error" : "Query 'id' is missing"})
-	}else if len(lang) == 0 {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error" : "Query 'lang' is missing"})
-	}else{
-		return true
+	if len(id) == 0 || len(lang) == 0 {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error" : "Query 'id' or 'lang' is missing"})
+		return false
 	}
-	return false
+	return true
 }
